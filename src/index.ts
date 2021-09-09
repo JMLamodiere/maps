b0168736a67e436238844b4752a7128afeb5d635
Jean-Marie Lamodière <JMLamodiere@users.noreply.github.com>
2021-09-09 16:34:02 +0200

    feat: add google maps using env var for key

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 // import { User } from "./User";
 // import { Company } from "./Company";
 
-new google.maps.Map(document.getElementById("map"), {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0,
-  },
-});
+const googleMapsSrc: string =
+  "https://maps.googleapis.com/maps/api/js?key=" +
+  process.env.GOOGLE_UDEMY_TS_KEY;
+const googleMapsScript = document.createElement("script");
+googleMapsScript.setAttribute("src", googleMapsSrc);
+document.body.appendChild(googleMapsScript);
+
+googleMapsScript.onload = function () {
+  new google.maps.Map(document.getElementById("map"), {
+    zoom: 3,
+    center: {
+      lat: 0,
+      lng: 0,
+    },
+  });
+};
